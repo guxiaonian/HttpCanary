53a06a0b06dc52116449b3a6baa4bd4b62fe80fb
gunaonian <gunaonian@xiaomi.com>
2020-08-04 11:08:59 +0800

    add setting

--- httpcanary-androidx/src/main/java/fairy/easy/httpcanary/HttpCanary.java
+++ httpcanary-androidx/src/main/java/fairy/easy/httpcanary/HttpCanary.java
@@ -3,6 +3,7 @@ package fairy.easy.httpcanary;
 import android.content.Context;
 
 import fairy.easy.httpcanary.preview.PreviewActivity;
+import fairy.easy.httpcanary.preview.SettingActivity;
 
 public class HttpCanary {
 
@@ -13,7 +14,7 @@ public class HttpCanary {
     private static HttpCanaryFactory httpCanaryFactory;
 
     static void install(Context context) {
-        HttpModel.setEnabled(context, PreviewActivity.class, true);
+        HttpModel.setEnabled(context, SettingActivity.class, true);
         httpCanaryFactory = new HttpCanaryFactory(context);
 
     }
--- httpcanary-androidx/src/main/java/fairy/easy/httpcanary/preview/SettingActivity.java
+++ httpcanary-androidx/src/main/java/fairy/easy/httpcanary/preview/SettingActivity.java
@@ -1,10 +1,14 @@
 package fairy.easy.httpcanary.preview;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import fairy.easy.httpcanary.R;
+import fairy.easy.httpcanary.util.PermissionsUtils;
 
 /**
  * 设置相关页面
@@ -15,9 +19,31 @@ import fairy.easy.httpcanary.R;
  */
 public class SettingActivity extends AppCompatActivity {
 
+    private Button btnPermission;
+    private Button btnDownload;
+    private Button btnSu;
+    private Button btnMigration;
+    private Button btnGo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.http_canary_activity_setting);
+        btnPermission = findViewById(R.id.http_canary_permission_btn);
+        btnDownload = findViewById(R.id.http_canary_download_btn);
+        btnSu = findViewById(R.id.http_canary_su_btn);
+        btnMigration = findViewById(R.id.http_canary_etc_btn);
+        btnGo = findViewById(R.id.http_canary_go_btn);
+        if (PermissionsUtils.checkPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            btnPermission.setEnabled(false);
+            btnDownload.setEnabled(true);
+        }
+
+        btnPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
